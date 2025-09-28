2dd2891bd19920a81e28eb14d1f15240381538a1
Arthur Espartinez <artespar85@gmail.com>
2025-09-28 17:59:12 +0800

    Test

--- src/main/java/com/example/customerservice/dto/SavingsDto.java
+++ src/main/java/com/example/customerservice/dto/SavingsDto.java
@@ -8,9 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SavingsDto {
-	
 	private Long accountNumber;
     private String accountType;
     private double availableBalance;
-
 }
